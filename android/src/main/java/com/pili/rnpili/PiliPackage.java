7018c287370c0578c8271d0cb916426bfd93568e
Brian Kereszturi <briankereszturi@gmail.com>
2018-02-20 19:04:00 -0800

    Remove unused call to createJSModules()

--- android/src/main/java/com/pili/rnpili/PiliPackage.java
+++ android/src/main/java/com/pili/rnpili/PiliPackage.java
@@ -31,11 +31,6 @@ public class PiliPackage implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Arrays.<ViewManager>asList(
                 new PiliStreamingViewManager(), //TODO PlayerViewManager , HC ,SC ,Audio
